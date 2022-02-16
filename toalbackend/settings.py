fca073867abcfce838f21984e4358552e2266932
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2022-02-16 17:57:08 +0300

    Add corsheaders middleware.

--- toalbackend/settings.py
+++ toalbackend/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'toalbackend.urls'
