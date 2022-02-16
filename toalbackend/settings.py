3fbc407762f92192c96c2d714473122d5b69936b
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2022-02-16 17:12:55 +0300

    Set the default renderer JSONRenderer.

--- toalbackend/settings.py
+++ toalbackend/settings.py
@@ -132,5 +132,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
